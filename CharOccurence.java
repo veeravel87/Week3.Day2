d7d63c82f76df1f3d01bd62d32b1e04547158e3d
veeravel87 <60774489+veeravel87@users.noreply.github.com>
2021-10-22 18:43:40 +0530

    Add files via upload

---
+++ CharOccurence.java
@@ -0,0 +1,25 @@
+package Week3.day2;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class CharOccurence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String companyName="Amazon India Development Centre";
+		char[] charArray=companyName.toCharArray();
+		Map<Character, Integer> map=new HashMap<Character, Integer>();
+		for (int i = 0; i < charArray.length; i++) {
+			if(map.containsKey(charArray[i]))
+				map.put(charArray[i], map.get(charArray[i])+1);
+			else
+				map.put(charArray[i], 1);
+			
+		}
+		System.out.println(map);
+
+	}
+
+}
